ef867f36c9182ec10b230b422fd89b05884be8da
P4rty <41465516+P4rty@users.noreply.github.com>
2024-01-23 14:11:57 +0900

    Title: 학점 계산기, Time: 177ms, Memory: 72MB, Status: Passed - CodeTree

---
+++ 240123/학점 계산기/credit-calculator.py
@@ -0,0 +1,15 @@
+n= int(input())
+
+arr = list(map(float, input().split()))
+
+sum_val = sum(arr)
+avg = sum_val/n
+
+print("{0:.1f}".format(avg))
+if avg >=4.0:
+    print("Perfect")
+elif avg >=3.0:
+    print("Good")
+else:
+    print("Poor"
+    )
