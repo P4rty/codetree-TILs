2075a6d10c3a566a99b440b77625f2fd340a6a8f
P4rty <41465516+P4rty@users.noreply.github.com>
2024-01-24 20:18:09 +0900

    Title: 점수대 파악하기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240124/점수대 파악하기/find-out-the-score-range.py
+++ 240124/점수대 파악하기/find-out-the-score-range.py
@@ -1,8 +1,8 @@
 a = list(map(int,input().split()))
 cnt = [0]*10
 for elem in a:
-    elem = elem//10
-    cnt[elem-1] +=1
-
+    if elem //10 != 0:
+        elem = elem//10
+        cnt[elem-1] +=1
 for i in range(10,0,-1):
     print(f'{i*10} - {cnt[i-1]}')
