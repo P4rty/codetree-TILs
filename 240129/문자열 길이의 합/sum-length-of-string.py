a2c5bede9a1d08975ffe6a7fc078630a99d8eab5
P4rty <41465516+P4rty@users.noreply.github.com>
2024-01-29 12:28:23 +0900

    Title: 문자열 길이의 합, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240129/문자열 길이의 합/sum-length-of-string.py
+++ 240129/문자열 길이의 합/sum-length-of-string.py
@@ -7,7 +7,7 @@ arr=[
 acnt= 0
 cnt = 0
 for i in range(n):
-        cnt+=len(arr[i])
-        if 'a' in arr[i]:
-            acnt+=1
+    cnt+=len(arr[i])
+    if 'a' in arr[i][0]:
+        acnt+=1
 print(f'{cnt} {acnt}')
