492be582321cabe01b0909a8f7b6c67b16e5e0af
P4rty <41465516+P4rty@users.noreply.github.com>
2024-01-31 13:34:28 +0900

    Title: 소문자와 숫자, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240131/소문자와 숫자/letter-and-number.py
@@ -0,0 +1,12 @@
+s = input()
+arr = []
+for i in range(len(s)):
+    if s[i] <= '9' and s[i]>= '1':
+        arr.append(s[i])
+    elif s[i] <= 'z' and s[i]>= 'a':
+        arr.append(s[i])
+    elif s[i] <= 'Z' and s[i]>= 'A':
+        c = chr(ord(s[i]) - ord('A')+ord('a'))
+        arr.append(c)
+s = ''.join(arr)
+print(s)
