da632e6b1282262410ea55f1f7ee8bcac831eb8a
P4rty <41465516+P4rty@users.noreply.github.com>
2024-01-31 17:12:38 +0900

    Title: 정수만 추출하기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240131/정수만 추출하기/extract-only-integers.py
+++ 240131/정수만 추출하기/extract-only-integers.py
@@ -4,12 +4,15 @@ a,b = input().split()
 for i in range(len(a)):
     if a[i]>='0' and a[i]<='9':
         arr1.append(a[i])
-
+    else:
+        break
 for i in range(len(b)):
     if b[i]>='0' and b[i]<='9':
         arr2.append(b[i])
+    else:
+        break
 a = ''.join(arr1)
 b = ''.join(arr2)
 a = int(a)
 b = int(b)
-print((a+b)//100)
+print(a+b)
