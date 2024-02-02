cf9a0114883d442f7038f3994fd8b3dc69c26dd0
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-02 11:49:50 +0900

    Title: 함수를 이용한 369 게임, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

--- 240202/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240202/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,19 +1,11 @@
-def find369(a,b):
-    cnt = 0
-    for i in range(a,b+1):
-        if i % 3 ==0:
-            cnt += 1
-        elif i%10 == 3 or i % 10 == 6 or i% 10 == 9:
-            cnt += 1
-        elif i//10 == 3 or i//10 == 6 or i//10 == 9:
-            cnt += 1
-        elif i//100 == 3 or i//100 == 6 or i//100 == 9:
-            cnt+=1
-        elif i//1000 == 3 or i//1000 == 6 or i//1000 == 9:
-            cnt+=1
-        elif i//10000 == 3 or i//10000 == 6 or i//10000 == 9:
-            cnt+=1
-        
-    return cnt
+def count_numbers(a, b):
+    count = 0
+    
+    for num in range(a, b + 1):
+        if '3' in str(num) or '6' in str(num) or '9' in str(num) or num % 3 == 0:
+            count += 1
+            
+    return count
+
 a,b = map(int,input().split())
-print(find369(a,b))
+print(count_numbers(a,b))
