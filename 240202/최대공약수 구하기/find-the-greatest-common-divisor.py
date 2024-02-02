3139b83a51afcb94a1ce410684739ec789571426
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-02 11:24:48 +0900

    Title: 최대공약수 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240202/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240202/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -2,10 +2,9 @@ def gcdd (n,m):
     for i in range(2,n):
         if n%i == 0 and m%i ==0:
             gcd = i
-    return gcd
+    print(gcd)
 
 
 
 n,m = map(int,input().split())
-
-print(gcdd(n,m))
+gcdd(n,m)
