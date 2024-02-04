d098fc182a4ee09210f6bdbba1420bbe47d58a76
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-05 08:52:45 +0900

    Title: 함수를 이용한 윤년 판별, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240205/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 240205/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,12 +1,10 @@
 def is_yoonnun(n):
     if n % 4 == 0:
+        if n % 100 == 0:
+            if n % 400 == 0:
+                return True
+            return False
         return True
-    elif n % 4 == 0 and n % 100 == 0:
-        return False
-    elif n % 4 == 0 and n % 100 == 0 and n % 400 == 0:
-        return True
-    return False
-
 y = int(input())
 if y:
     print('true')
