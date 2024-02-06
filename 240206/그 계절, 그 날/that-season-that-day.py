dd28da568c2268be8e604e9633eac83bd456f9c3
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-06 14:16:36 +0900

    Title: 그 계절, 그 날, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240206/그 계절, 그 날/that-season-that-day.py
+++ 240206/그 계절, 그 날/that-season-that-day.py
@@ -11,40 +11,54 @@ def is_yoon(n):
         return False
 
 def month_check(t,M,D):
-    if t :
+    # if t :
+    #     if M == 2:
+    #         if 1 <= D <=29:
+    #             return True
+    #         else:
+    #             return False
+    #     if M in (1,3,5,7,8,10,12):
+    #         if 1 <= D <= 31:
+    #             return True
+    #         else:
+    #             return False
+    #     elif M in (4, 6, 9, 11):
+    #         if 1 <= D <= 30:
+    #             return True
+    #         else:
+    #             return False
+    # else:
+    #     if M == 2:
+    #         if 1 <= D <= 28:
+    #             return True
+    #     else:
+    #         return False
+    #     if M in (1,3,5,7,8,10,12):
+    #         if 1 <= D <= 31:
+    #             return True
+    #         else:
+    #             return False
+    #     elif M in (4, 6, 9, 11):
+    #         if 1 <= D <= 30:
+    #             return True
+    #         else:
+    #             return False
+    if t:
         if M == 2:
-            if 1 <= D <=29:
-                return True
-            else:
-                return False
-        if M in (1,3,5,7,8,10,12):
-            if 1 <= D <= 31:
-                return True
-            else:
-                return False
+            return 1 <= D <= 29
+        elif M in (1, 3, 5, 7, 8, 10, 12):
+            return 1 <= D <= 31
         elif M in (4, 6, 9, 11):
-            if 1 <= D <= 30:
-                return True
-            else:
-                return False
+            return 1 <= D <= 30
     else:
         if M == 2:
-            if 1 <= D <= 28:
-                return True
-        else:
-            return False
-        if M in (1,3,5,7,8,10,12):
-            if 1 <= D <= 31:
-                return True
-            else:
-                return False
+            return 1 <= D <= 28
+        elif M in (1, 3, 5, 7, 8, 10, 12):
+            return 1 <= D <= 31
         elif M in (4, 6, 9, 11):
-            if 1 <= D <= 30:
-                return True
-            else:
-                return False
+            return 1 <= D <= 30
 def is_season(t,M):
-    if t == 1:
+    if t:
         if M in (3,4,5):
             print('Spring')
         if M in (6,7,8):
