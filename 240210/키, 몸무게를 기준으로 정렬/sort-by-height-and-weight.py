81a4e1d9c1c0a8eee4e488ec46393d1c29ca979c
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-10 10:25:35 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 198ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240210/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.py
@@ -0,0 +1,15 @@
+n = int (input())
+
+class Person:
+    def __init__(self,name,height,weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+people = []
+for _ in range(n):
+    name,height,weight = tuple(input().split())
+    people.append(Person(name,int(height),int(weight)))
+people.sort(key= lambda x:(x.height,-x.weight))
+
+for i in range(n):
+    print(people[i].name,people[i].height,people[i].weight)
