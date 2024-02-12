f64d5922a6c27a2f82d4d490032a670a3add69ce
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-12 20:41:06 +0900

    Title: 요일 맞추기, Time: 5069ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

--- 240212/요일 맞추기/guess-day-of-week.py
+++ 240212/요일 맞추기/guess-day-of-week.py
@@ -1,23 +1,34 @@
-def day_of_week(m, d):
-    # 2011년 1월 1일은 토요일이므로, 토요일을 0으로 기준으로 요일을 계산
-    days_in_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    total_days = (m - 1) * 31 + d - 1
+days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-    for i in range(1, m):
-        total_days -= days_in_month[i]
-
-    day_of_week = (total_days + 6) % 7
-    return day_of_week
-
-# 입력값 받기
 m1, d1, m2, d2 = map(int, input().split())
 
-# 시작 날짜의 요일 계산 (m1월 d1일이 월요일이라고 가정)
+
 start_day = 0
+elapsed_days = 0
+if m1< m2:
+    while True:
+        if m1 == m2 and d1 == d2:
+            break
 
-# 목표 날짜의 요일 계산
-target_day = (start_day + (d2 - d1)) % 7
+        elapsed_days += 1
+        d1 += 1
 
-# 요일 출력
-days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-print(days_of_week[target_day])
+        if d1 > num_of_days[m1]:
+            m1 += 1
+            d1 = 1
+else:
+     while True:
+        if m1 == m2 and d1 == d2:
+            break
+
+        elapsed_days -= 1
+        d1 -= 1
+
+        if d1 ==1:
+            m1 -= 1
+            d1 = num_of_days[m1]
+f = elapsed_days % 7
+
+
+print(days_of_week[f])
