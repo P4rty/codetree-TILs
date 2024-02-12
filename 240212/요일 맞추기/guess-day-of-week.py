c8c7cf84e9ee0207b7c99a2087d31fbb552a6713
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-12 20:27:41 +0900

    Title: 요일 맞추기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240212/요일 맞추기/guess-day-of-week.py
+++ 240212/요일 맞추기/guess-day-of-week.py
@@ -3,21 +3,16 @@ def day_of_week(m, d):
     days_in_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     total_days = (m - 1) * 31 + d - 1
 
-    for i in range(1, m):
-        total_days -= days_in_month[i]
+    return total_days
 
-    day_of_week = (total_days + 6) % 7
-    return day_of_week
-
-# 입력값 받기
 m1, d1, m2, d2 = map(int, input().split())
 
-# 시작 날짜의 요일 계산
+
 start_day = day_of_week(m1, d1)
 
-# 목표 날짜의 요일 계산
-target_day = (start_day + (d2 - d1)+3) % 7
 
-# 요일 출력
-days_of_week = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"]
-print(days_of_week[target_day])
+target_day = day_of_week(m2, d2)
+
+days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri","Sat", "Sun"]
+f = (target_day-start_day)%7
+print(days_of_week[f])
