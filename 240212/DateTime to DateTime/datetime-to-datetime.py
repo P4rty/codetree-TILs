6b10099e490cc3faf14ddafb243eaa4df95ed8c2
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-12 19:57:10 +0900

    Title: DateTime to DateTime, Time: 5015ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 240212/DateTime to DateTime/datetime-to-datetime.py
+++ 240212/DateTime to DateTime/datetime-to-datetime.py
@@ -2,24 +2,13 @@ a,b,c = map(int,input().split())
 d,e,f = 11, 11, 11
 elapsed = 0
 
-while True:
-    if d == a and e == b and  f == c:
-
-        break
-    if f!= c:
-        elapsed += 1
-        f += 1
-    elif e != b:
-        elapsed +=60
-        e +=1
-    
+while (d,e,f) != (a,b,c):
+    elapsed += 1
+    f += 1
     if f == 60:
         e += 1
         f = 0
     if e == 24:
         d += 1
         e = 0
-        f = 0
-        
-    
 print(elapsed)
