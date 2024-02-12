5f1cabdc94ac011b2c58916b251b19a458e5ab42
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-12 20:00:24 +0900

    Title: DateTime to DateTime, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240212/DateTime to DateTime/datetime-to-datetime.py
+++ 240212/DateTime to DateTime/datetime-to-datetime.py
@@ -5,7 +5,7 @@ target_time = (a, b, c)
 start_minutes = start_time[0] * 24 * 60 + start_time[1] * 60 + start_time[2]
 target_minutes = target_time[0] * 24 * 60 + target_time[1] * 60 + target_time[2]
 
-if target_minutes <= start_minutes:
+if target_minutes < start_minutes:
     print(-1)
 else:
     elapsed_minutes = target_minutes - start_minutes
