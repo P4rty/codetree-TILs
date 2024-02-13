70312ea91d8d950c058a0fb52448cb7b791a49df
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-13 10:11:03 +0900

    Title: 그 요일은, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240213/그 요일은/the-day-of-the-day.py
+++ 240213/그 요일은/the-day-of-the-day.py
@@ -2,11 +2,6 @@ days_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 m1,d1,m2,d2 = map(int,input().split())
 A = input()
 
-
-for i in range(7):
-    if A == days_week[i]:
-        f = i
-
 def num_of_days(m, d):
     days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     total_days = 0
@@ -18,15 +13,10 @@ def num_of_days(m, d):
     
     return total_days    
 
-
+total_days_start = num_of_days(m1, d1)
 total_days = (num_of_days(m2, d2) - num_of_days(m1, d1))
-total_week = total_days //7 + 1
-total_day = total_days % 7
+total_week = total_days //7 +1
+start_day_index = days_week.index(A)
 
-if days_week.index(A)+1 <= total_day and total_days > 7:
-    print(total_week+1)
-elif total_days < 7:
-    if days_week.index(A)+1 <= total_day:
-        print(1)
-else:
-    print(total_week)
+occurrences = total_week+ (start_day_index <= (total_days_start % 7))
+print(occurrences)
