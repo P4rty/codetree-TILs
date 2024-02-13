91b95f8116de4114a029041a31deb1a6e47a234d
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-13 12:23:15 +0900

    Title: 왔다 갔던 구역 2, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240213/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,28 @@
+n = int(input())
+arr = [0]*2001
+idx = 1000
+for i in range(n):
+    x,direction = input().split()
+    x = int(x)
+    if direction == "L":
+        for j in range(idx,x+1):
+            arr[j] +=1
+        idx = x+1
+    elif direction == "R":
+        for j in range(x,idx+1):
+            arr[j] +=1
+        idx = x
+
+ck = 0
+for i in range(len(arr)):
+    if arr[i] >= 2:
+        arr[i] = 1
+    else:
+        arr[i] = 0
+mk = 0
+for i in range(len(arr)):
+    if arr[i] == 1:
+        ck += 1
+    if arr [i] != arr[i-1]:
+        ck -= 1
+print(ck-1)
