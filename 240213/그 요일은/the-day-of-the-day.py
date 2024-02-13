528a47a6713aa0db9466f922f2ceb0e98da55c61
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-13 10:13:58 +0900

    Title: 그 요일은, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240213/그 요일은/the-day-of-the-day.py
+++ 240213/그 요일은/the-day-of-the-day.py
@@ -3,7 +3,7 @@ m1,d1,m2,d2 = map(int,input().split())
 A = input()
 
 def num_of_days(m, d):
-    days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     total_days = 0
     
     for i in range(1, m):
@@ -14,7 +14,7 @@ def num_of_days(m, d):
     return total_days    
 
 total_days = (num_of_days(m2, d2) - num_of_days(m1, d1))
-total_week = total_days //7 +1
+total_week = total_days //7
 start_day_index = days_week.index(A)
 
 occurrences = total_week+ (start_day_index <= (total_days % 7))
