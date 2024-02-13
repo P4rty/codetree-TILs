05f2d20c277dbe69c896ba0399f56073551483f1
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-13 12:34:05 +0900

    Title: 왔다 갔던 구역 2, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240213/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240213/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -5,24 +5,11 @@ for i in range(n):
     x,direction = input().split()
     x = int(x)+1000
     if direction == "L":
-        for j in range(idx,x+1):
-            arr[j] +=1
-        idx = x+1
+        arr[idx:x] = [val + 1 for val in arr[idx:x]]
+        idx = x + 1
     elif direction == "R":
-        for j in range(x,idx+1):
-            arr[j] +=1
+        arr[x:idx] = [val + 1 for val in arr[x:idx]]
         idx = x
 
-ck = 0
-for i in range(len(arr)):
-    if arr[i] >= 2:
-        arr[i] = 1
-    else:
-        arr[i] = 0
-mk = 0
-for i in range(len(arr)):
-    if arr[i] == 1:
-        ck += 1
-    if arr [i] != arr[i-1]:
-        ck -= 1
-print(ck)
+ck = sum(1 for val in arr if val >= 2)
+print(ck-1)
