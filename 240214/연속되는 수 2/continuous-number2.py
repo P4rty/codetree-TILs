d6bc7a0d8ddc67f237383bb1e4ddc09ac5c1c312
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-14 12:11:31 +0900

    Title: 연속되는 수 2, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240214/연속되는 수 2/continuous-number2.py
+++ 240214/연속되는 수 2/continuous-number2.py
@@ -12,7 +12,9 @@ cnt = 1
 fma = 1
 for i in range(N):
     if arr[i] == arr[i-1]:
-        cnt +=1
+        cnt += 1
+        if arr[i]==N-1:
+            fma = cnt -1
     if arr[i] != arr[i-1]:
         if cnt > fma :
             fma = cnt
