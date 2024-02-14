00fd7a71b746d51b10dcb95709150fa71ba4aaf3
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-14 12:06:36 +0900

    Title: 연속되는 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240214/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,15 @@
+N = int(input())
+arr = []
+for i in range(N):
+    num = int(input())
+    arr.append(num)
+cnt = 1
+fin = []
+for i in range(N):
+    if arr[i] == arr[i-1]:
+        cnt +=1
+    if arr[i] != arr[i-1]:
+        fin.append(cnt)
+        cnt = 1
+
+print(max(fin))
