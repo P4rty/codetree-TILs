14e739e845ac7a203060c04a4d3070cf35d80dfc
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-15 16:42:07 +0900

    Title: 선두를 지켜라, Time: 137ms, Memory: 40MB, Status: Wrong Answer - CodeTree

--- 240215/선두를 지켜라/keep-the-lead.py
+++ 240215/선두를 지켜라/keep-the-lead.py
@@ -17,17 +17,31 @@ for i in range(m):
     for j in range(time,time+t):
         pos_b[j] = v +pos_b[j-1] 
     time +=t
-leader_changes = -1
-direction_A = 1
-direction_B = 1
+# leader_changes = 0
+# direction_A = 1
+# direction_B = 1
 
-for k in range(1, time):
-    if pos_a[k] > pos_b[k] and direction_A == 1:
-        direction_A = -1
-        leader_changes += 1
-        direction_B = 1
-    elif pos_a[k] < pos_b[k] and direction_B == 1:
-        direction_B = -1
-        leader_changes += 1
-        direction_A = 1
-print(leader_changes)
+# # for k in range(0, time):
+# #     if pos_a[k] > pos_b[k] and direction_A == 1:
+# #         direction_A = -1
+# #         leader_changes += 1
+# #         direction_B = 1
+# #     elif pos_a[k] < pos_b[k] and direction_B == 1:
+# #         direction_B = -1
+# #         leader_changes += 1
+# #         direction_A = 1
+# # print(leader_changes)
+arr = []
+direction = 1
+for k in range(1,time+1):
+    if pos_a[k] >= pos_b[k]:
+        direction = 1
+        arr.append(direction)
+    elif pos_a[k] < pos_b[k]:
+        direction = -1
+        arr.append(direction)
+cnt = -1
+for k in range(1,len(arr)):
+    if arr[k] != arr[k-1]:
+        cnt+=1
+print(cnt)
