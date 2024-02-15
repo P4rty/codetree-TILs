443711f08bda067c5ecb15c807987afa926ba115
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-15 15:24:54 +0900

    Title: 벌금은 누구에게, Time: 130ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240215/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,14 @@
+import sys
+def input():
+    return sys.stdin.readline().rstrip()
+n,m,k = map(int,input().split())
+studentscore = [0]*(n+1)
+cnt = 0
+for i in range(m):
+    s = int(input())
+    studentscore[s] += 1
+    if studentscore[s] >= k:
+        cnt +=1
+        print(s)
+if cnt == 0 :
+    print(-1)
