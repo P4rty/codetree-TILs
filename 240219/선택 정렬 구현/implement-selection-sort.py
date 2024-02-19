cd7046459aaf886125263885bc9196c23afc1ac2
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-19 12:12:50 +0900

    Title: 선택 정렬 구현, Time: 153ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240219/선택 정렬 구현/implement-selection-sort.py
+++ 240219/선택 정렬 구현/implement-selection-sort.py
@@ -1,17 +1,12 @@
-def selection_sort(arr):
-    len_arr = len(arr)
-    for i in range(len_arr-1):
-        min = i
-        for j in range(i+1,len_arr):
-            if arr[j] < arr[min]:
-                min = j
-            tmp = arr[i]
-            arr[i] = arr[min]
-            arr[min] = tmp
-    return arr
-
 n = int(input())
 arr = list(input().split())
-selection_sort(arr)
+for i in range(len(arr)-1):
+    min_idx = i
+    for j in range(i+1,len(arr)):
+        if arr[j] < arr[min_idx]:
+            min_idx = j
+    tmp = arr[i]
+    arr[i] = arr[min_idx]
+    arr[min_idx] = tmp
 for i in range(len(arr)):
     print(arr[i],end=" ")
