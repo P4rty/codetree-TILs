3e41de13d26ee9f6676b176f9d682cdd1f5d8e84
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-20 14:04:36 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 115ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 240220/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -0,0 +1,9 @@
+s = list(input())
+
+cnt = 0
+for i in range(len(s)):
+    if s[i] == '(':
+        for j in range(i + 1, len(s)):
+            if s[j] == ')':
+                cnt += 1                
+print(cnt)
