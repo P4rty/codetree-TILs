ccf5257d22d8d807133dd70673068b90aa7707ac
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-20 15:02:34 +0900

    Title: 일렬로 서있는 소 2, Time: 156ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 240220/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,10 @@
+n= int(input())
+m = list(map(int,input().split()))
+cnt = 0
+for i in range(n):
+    for j in range(i+1,n):
+        for k in range(j+1,n):
+            if m[i] <= m[j] and m[j] <=m[k]:
+                cnt += 1
+
+print(cnt)
