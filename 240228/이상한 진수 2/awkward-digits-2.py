a0c9d7f7760f0966dc8a2028271396dab3599140
P4rty <41465516+P4rty@users.noreply.github.com>
2024-02-28 16:49:43 +0900

    Title: 이상한 진수 2, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240228/이상한 진수 2/awkward-digits-2.py
+++ 240228/이상한 진수 2/awkward-digits-2.py
@@ -5,7 +5,7 @@ for i in range(n):
     a[i] = int(a[i])
 
 cnt = True
-for i in range(n-1):
+for i in range(n):
     if cnt == True and a[i] == 0 :
         a[i] = 1
         cnt = False
@@ -14,10 +14,7 @@ if cnt == True and a[n-1] == 1:
 elif cnt == True and a[n-1] == 0:
     a[n-1] = 1
 dec = 0
-for i in range(n-1):
-    dec += 2**(n-1-i)
-
-if a[n-1] == 1:
-    dec+=1
+for i in range(n):
+    dec += 2**(n-1-i)*a[i]
 
 print(dec)
